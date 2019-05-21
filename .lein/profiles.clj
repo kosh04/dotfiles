7c1b84512957f45937ea2eec93e02d58ed79820d
kosh04 <shigeru.kb@gmail.com>
2019-05-21 21:53:33 +0900

    Add leiningen profiles

---
+++ .lein/profiles.clj
@@ -0,0 +1,16 @@
+;;; .lein/profiles.clj
+
+;; link:
+;; - https://github.com/technomancy/leiningen/blob/master/doc/PROFILES.md
+;; - https://github.com/clojure-emacs/cider#setting-up-ciders-nrepl-middleware
+
+{:repl
+ {:plugins [[cider/cider-nrepl "0.9.1"]
+            [lein-pprint "1.1.1"]
+            [lein-auto "0.1.2"]
+            [lein-kibit "0.1.2"]
+            [refactor-nrepl "1.1.0"]]
+  :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                 [org.clojure/tools.cli "0.3.3"]
+                 [org.clojure/math.combinatorics "0.1.1"]
+                 [slamhound "1.3.1"]]}}
