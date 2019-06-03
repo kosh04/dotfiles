bb47a648bf91442ade359d23206da0e53a1181ce
kosh04 <shigeru.kb@gmail.com>
2019-06-04 02:03:56 +0900

    Few changes

--- .config/homebrew/bundle.sh
+++ .config/homebrew/bundle.sh
@@ -4,13 +4,13 @@ set -eu
 : "${BREWFILE:=$HOME/.config/homebrew/Brewfile}"
 
 usage="Usage: $0 [install|dump] env:BREWFILE=${BREWFILE}"
-command=${1?$usage}
+command=${1-help}
 
 case $command in
     install)
         brew bundle install --verbose --file="$BREWFILE" ;;
     dump)
         brew bundle dump --describe --file="$BREWFILE" ;;
-    *)
+    help|*)
         echo 1>&2 "$usage" ;;  
 esac
