dbaaff5ef8963bb8c2c0a614fe02e4dc40d9c1f5
kosh04 <shigeru.kb@gmail.com>
2019-06-14 21:42:08 +0900

    Update debian

---
+++ .config/python/pythonstartup.py
@@ -0,0 +1,11 @@
+# export PYTHONSTARTUP=~/.pythonstartup.py
+# python -i ~/.pythonstartup.py
+
+# https://docs.python.org/ja/3/howto/pyporting.html
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import generators
+from __future__ import print_function
+from __future__ import unicode_literals
+
+print('loading pythonstartup...done')
--- .pythonstartup.py
+++
@@ -1,4 +0,0 @@
-# export PYTHONSTARTUP=~/.pythonstartup.py
-# python -i ~/.pythonstartup.py
-
-print('loading pythonstartup...done')
