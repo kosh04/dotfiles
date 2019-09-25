4c91b54c8d71a9ec5ee0a2a307ae108ca25e5ce8
kosh04 <shigeru.kb@gmail.com>
2019-09-26 02:22:47 +0900

    Extend flag options

--- bin/psgrep.sh
+++ bin/psgrep.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 set -eu -o pipefail
 
-pattern=${1?"Usage: $0 PATTERN"}
+usage="Usage: $0 [options] PATTERN"
+
+pgrepopt=
 xargsopt=
+
+while getopts ix OPT
+do
+    case $OPT in
+        i) pgrepopt+="-i " ;;
+        x) pgrepopt+="-x " ;;
+        *) ;;
+    esac
+done
+shift $((OPTIND - 1))
 
 case "$OSTYPE" in
     linux-gnu) xargsopt=--no-run-if-empty ;; # or "-r"
 esac
 
-pgrep -d, -ix "$pattern" | xargs $xargsopt ps -fp
+pattern=${1?"$usage"}
+
+pgrep -d, $pgrepopt "$pattern" | xargs $xargsopt ps -fp
