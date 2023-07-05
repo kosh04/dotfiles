7176969ba53fe1a7206cf69e7a58625c7091abe1
kosh04 <shigeru.kb@gmail.com>
2023-07-05 21:45:33 +0900

    windows-terminal config moved to OneDrive

--- .config/windows-terminal/install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-setlocal
-set LINK=%LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json
-set TARGET=%~dp0settings.json
-
-mklink /h "%LINK%" "%TARGET%"
-
